b6183ebe8cb3017f0cb70a834c4cf127b4bca652
nizamiki60 <nizamiki60@gmail.com>
2023-12-10 11:34:57 +0700

    first init github

---
+++ LapnganFutsal/cara-boking.php
@@ -0,0 +1,26 @@
+   <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-8">
+                <div class="page-section">
+                    <div class="width-350 width-300-md width-100pc-xs paragraph-inline">
+                        <div class="embed-responsive embed-responsive-16by9">
+                          <img src='images/profil.jpg'>
+                        </div>
+                    </div>
+                    <p>
+                        <strong>Cara Boking Online</strong>
+                    </p>
+                    <p>1. Masuk ke website</p>
+                    <p>2. Daftar sebagai member</p>
+                    <p>3. Milih jadwal jam yang kosong yang tersedia</p>
+                    <p>4. Boking</p>
+                    <p>5. Selesaikan Transaksi</P>
+                   
+                </div>
+               
+                    <!-- // END .panel -->
+                </div>
+                <!-- // END .page-section -->
+            </div>
+        </div>
+    </div>
---
+++ LapnganFutsal/profil.php
@@ -0,0 +1,30 @@
+   <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-8">
+                <div class="page-section">
+                    <div class="width-350 width-300-md width-100pc-xs paragraph-inline">
+                        <div class="embed-responsive embed-responsive-16by9">
+                          <img src='images/profil.jpeg'>
+                        </div>
+                    </div>
+                    <p>
+                        <strong>Profil lapangan Futsal</strong>
+                    </p>
+                    <p>Profil Usaha Lapangan Futsal</p>
+                    <p>Nama Usaha : Vigor Futsal</p>
+                    <p>Jenis Usaha : Lapangan/Tempat Futsal</p>
+                    <p>Pemilik : Firman Syahrul Nizam (seorang Mahasiswa)</p>
+                    <p>Lokasi : Jl. Kamboja pati</p>
+                    <p>Sejarah : Didirikan pada tahun 2022</p>
+                    Vigor Futsal memiliki dua lapangan utama yang menjadi sumber pendapatan usaha,dan ruko sebagai tempat jualan makanan, dan minuman. hingga saat ini Andi futsal banyak dikunjungi oleh para Mahasiswa maupun Siswa Sekolah Menengah disekitar pati. Tujuannya adalah untuk memfasilitasi mahasiswa atau warga yang ingin berolahraga.    
+                    
+                
+                   
+                </div>
+               
+                    <!-- // END .panel -->
+                </div>
+                <!-- // END .page-section -->
+            </div>
+        </div>
+    
